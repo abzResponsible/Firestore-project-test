bd33ad12f5c087fe6da03d2db6f288d2341c79fb
Abdi Abyan <aa7872@ou.ac.uk>
2026-02-14 13:49:58 +0000

    Update firebase.config.js

--- src/services/firebase.config.js
+++ src/services/firebase.config.js
@@ -3,14 +3,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyC5u80wO6iaPl8E9auM0IRXliYGKyDQHfU",
-  authDomain: "todo-b74fc.firebaseapp.com",
-  projectId: "todo-b74fc",
-  storageBucket: "todo-b74fc.appspot.com",
-  messagingSenderId: "872116099545",
-  appId: "1:872116099545:web:9bb66d12ca15f2f39521c8"
+  apiKey: "AIzaSyCvJnY7uqorqJbSi_0YVcbi0QwK7VPUKqw",
+  authDomain: "todolocationapp-cb5e5.firebaseapp.com",
+  projectId: "todolocationapp-cb5e5",
+  storageBucket: "todolocationapp-cb5e5.firebasestorage.app",
+  messagingSenderId: "1637879775",
+  appId: "1:1637879775:web:ce974b3d2c958b48837e6a"
 };
 
 const app = initializeApp(firebaseConfig);
+
 
 export const db = getFirestore(app);
