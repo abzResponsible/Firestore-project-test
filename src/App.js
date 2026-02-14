ee031d4d53321eef55d15db5952b5ab599b8666d
Abdi Abyan <aa7872@ou.ac.uk>
2026-02-14 15:10:50 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Todo from './components/Todo';
+import Header from './components';
 
 function App() {
   return (
     <div className="App">
+    <Header />
       <Todo />
     </div>
   );
